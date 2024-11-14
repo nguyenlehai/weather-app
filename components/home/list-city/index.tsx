258c83d8643f0a83a53abfdc5ce404574eb4ec6d
HaiNguyenLe <nguyenlehai@hotmail.com>
2024-11-15 02:35:22 +0700

    feat: update icon weather and detail weather

--- components/home/list-city/index.tsx
+++ components/home/list-city/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import styles from '@/app/[locale]/home/home.module.css';
-import { kelvinToCelsius } from '@/lib/utils';
+import { getIconUrl, kelvinToCelsius } from '@/lib/utils';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { WeatherData } from '@/types/common';
+import Image from 'next/image';
 
 interface ListCityProps {
   item: WeatherData;
@@ -19,6 +20,15 @@ export const ListCity = ({ item, onHandleRemove, index }: ListCityProps) => {
     <>
       <div className={styles.weatherItemDetails}>
         <div className={styles.weatherItemTitle}>{item.name}</div>
+        <Image src={getIconUrl(item)} alt="icon" width={48} height={48} />
+        {item?.weather && item.weather.length > 0 && (
+          <div>
+            {t('weather', {
+              main: item.weather[0].main,
+              description: item.weather[0].description,
+            })}
+          </div>
+        )}
         {item.main?.temp && (
           <div>
             {t('temperature', {
